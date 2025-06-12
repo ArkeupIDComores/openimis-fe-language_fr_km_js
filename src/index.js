9aca82f773382c1c724a5fd8a51c27b43d91cc17
Maxime NGOE <maxime.ngoe@gmail.com>
2025-06-12 10:28:53 +0100

    Update index.js for translation key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import messages_km from "./translations/km.json";
 import flatten from "flat";
 
 const DEFAULT_CONFIG = {
-  "translations": [{ key: 'km', messages: flatten(messages_km) }],
+  "translations": [{ key: 'fr_KM', messages: flatten(messages_km) }],
 }
 
 export const LanguageKmModule = (cfg) => {
